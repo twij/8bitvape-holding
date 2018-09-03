b3452efd7a23b04e24c2b77d3d0c9112ada6897a
twij <twij@protonmail.com>
2018-09-03 23:28:35 +0100

    Basic holding page
    Just a basic holding page

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>8BitVape</title>
+  <meta name="description" content="8BitVape">
+  <meta name="author" content="twij">
+
+  <link rel="stylesheet" href="default.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <div class="container"><img src="img/logobfl.png" alt="8BitVape"><p>We're not dead yet..</p></div>
+</body>
+</html>
